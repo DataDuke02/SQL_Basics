fb0259d5197bff031d90f2291c8c3793f0eabb71
Thirugnansambantham S <thirugnanasambantham2802@gmail.com>
2026-05-07 22:29:28 +0530

    Creating Customers table
    Creating a table to store a details about the customers

---
+++ Customers.sql
@@ -0,0 +1,13 @@
+CREATE TABLE customers (
+	customer_id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50)
+);
+
+
+INSERT INTO customers (first_name, last_name)
+VALUES ("Fred","Fish"),
+	   ("Larry","Lobster"),
+       ("Bubble","Bass");
+
+SELECT * FROM customers;
